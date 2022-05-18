23bd90dc77e09d9d3a298e0004bc12f7c1faa91d
Oieusouopi <rafasc866@gmail.com>
2022-05-17 23:57:32 -0300

    tentando os testes dnv

--- test/unit/controllers/salesControllers.js
+++ test/unit/controllers/salesControllers.js
@@ -4,7 +4,7 @@ const sinon = require('sinon');
 
 const salesServices = require('../../../services/salesServices');
 const salesController = require('../../../controllers/salesController');
-const { arrayMockSalesModules } = require('../../helpers/mockSales');
+const { arrayMockSalesModules, validObjectModules } = require('../../helpers/mockSales');
 
 
 describe('teste para a função de pegar todas as vendas na controller', () => {
@@ -30,6 +30,36 @@ describe('teste para a função de pegar todas as vendas na controller', () => {
 
     it('se o response é uma array com valores correto', async () => {
         await salesController.getAllSales(req, res);
+        expect(res.json.calledWith(sinon.match.array)).to.be.equal(true);
+    });
+
+});
+
+describe('teste para a função de pegar uma venda pelo id na controller', () => {
+
+    const req = {};
+    const res = {};
+    const next = () => {};
+
+    beforeEach(() => {
+        res.status = sinon.stub().returns(res);
+        res.json = sinon.stub().returns()
+        req.params = { id: 1 };
+  
+        sinon.stub(salesServices, 'getIdSales').resolves([validObjectModules]);    
+    });
+
+    afterEach(() => {
+      salesServices.getIdSales.restore();
+    });
+
+    it('se o response é um status 200 com valores corretos', async () => {
+        await salesController.getIdSales(req, res, next);
+        expect(res.status.calledWith(200)).to.be.equal(true);
+    });
+
+    it('se o response é uma array com valores correto', async () => {
+        await salesController.getAllSales(req, res, next);
         expect(res.json.calledWith(sinon.match.array)).to.be.equal(true);
     });
 
